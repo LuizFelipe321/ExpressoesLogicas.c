2e340352733811ba301c82886a90f698b70a58cd
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2026-09-10 22:12:27 -0300

    Update Pratica0403.c

--- Pratica0403.c
+++ Pratica0403.c
@@ -2,21 +2,21 @@
 
 int main(){
 
-    float num;
+    float num; //declara variavel float
 
-    printf("Digite um numero:");
-    scanf("%f", &num);
+    printf("Digite um numero:"); //pede ao usuario para digitar um numero
+    scanf("%f", &num); // registra na variavel o valor digitado pelo usuario
 
-    if (num == 0){
-        printf("Esse numero eh nulo");
+    if (num == 0){ // condicao se varivel num receber do usuario valor = 0
+        printf("Esse numero eh nulo"); //exibe mensagem na tela
     }
-    if(num > 0){
-        printf("Numero positivo");
+    if(num > 0){ //se usuario digitou como num > 0
+        printf("Numero positivo"); //exibe mensagem na  tela
     }
-    if(num < 0){
+    if(num < 0){ // se num receber valor menor que 0
 
-        printf("Numero negativo");
+        printf("Numero negativo"); // exibe mensagem na tela
     }
 
-    return 0;
+    return 0; // encerra o programa
 }
