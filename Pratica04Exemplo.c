8f8803f054815b0fb31955e5fcbbf5b90db2fdbe
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2026-09-10 21:35:13 -0300

    Create Pratica04Exemplo.c

---
+++ Pratica04Exemplo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int A = 2;
+    int B = 3;
+    int MAIOR;
+
+    if ( A > B){
+
+        MAIOR = A;
+    } else {
+
+    MAIOR = B;
+
+    }
+
+    printf("Maior: %d", MAIOR);
+    return 0;
+}
